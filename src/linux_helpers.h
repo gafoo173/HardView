898b6e221acfceabb70158c0fde0c254f7ae0579
gafoo <gafaropop1010@gmail.com>
2025-07-12 06:15:17 +0300

    Update linux_helpers.h

--- src/linux_helpers.h
+++ src/linux_helpers.h
@@ -1,5 +1,6 @@
 #ifndef LINUX_HELPERS_H
 #define LINUX_HELPERS_H
+#include <stdbool.h>
 
 char* _read_dmi_attribute_linux(const char* attribute_name);
 char* _read_proc_sys_value(const char* path, const char* key);
